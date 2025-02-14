5a83db0ee6121c11e026fb000c64bc459081b8be
Zander Harding <zandernh@gmail.com>
2025-02-14 16:28:22 +0200

    Built some basic Task Creation and Reading functionality as part of CRUD

--- focushive_project/todo/urls.py
+++ focushive_project/todo/urls.py
@@ -6,6 +6,16 @@ urlpatterns = [
     path('', views.home),
     path('register', views.register),
     path('my-login', views.my_login),
+
+
+    # Crud Operations
+
+
+    # Create Task
+    path('create-task', views.createTask),
+
+    # Read Task
+    path('view-tasks', views.viewTask, name="view-tasks"),
 ]
 
 
--- focushive_project/todo/views.py
+++ focushive_project/todo/views.py
@@ -1,32 +1,12 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .models import Task
+from .forms import TaskForm
 
 # Create your views here.
 
 def home(request):
-
-    client_list = [
-        {
-            'id' : 1,
-            'name' : 'Zander Harding',
-            'profession' : 'Software Engineer'
-        },
-
-        {
-            'id' : 2,
-            'name': 'John Smith',
-            'profession' : 'Web developer'
-        },
-
-        {
-            'id' : 3,
-            'name': 'Luke Warren',
-            'profession' : 'Systems Architect'
-        }
-    ]
-
-    context = {'client': client_list}
-
-    return render(request, 'index.html', context=context)
+    return render(request, 'index.html')
 
 def register(request):
     return render(request, 'register.html')
@@ -34,3 +14,33 @@ def register(request):
 def my_login(request):
     return render(request, 'my-login.html')
 
+
+# Create a Task
+
+def createTask(request):
+
+    form = TaskForm()
+
+    if request.method == "POST":
+
+        form = TaskForm(request.POST)
+
+        if form.is_valid():
+
+            form.save()
+
+            return redirect("view-tasks")
+        
+    context = {'form' : form}
+
+    return render(request, 'create-task.html', context=context)
+
+# Read a Task
+
+def viewTask(request):
+
+    tasks = Task.objects.all()
+
+    context = {'tasks' : tasks}
+
+    return render(request, 'view-tasks.html', context=context)
